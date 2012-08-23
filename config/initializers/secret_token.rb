d3f98d7b17be2f5d4aee4925677c88ce71cb3d83
Ahmed Nadar <ahmedfay@hotmail.com>
2012-08-23 03:28:11 +0300

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webasil::Application.config.secret_token = '078321f1835d8d81fd4f0adccddc875f2e9ac47478f40d6afbe39387b5dfc6d0b50fcd1dc891e66a5fb414153bc00f8a0433d56b0669390d12d053a13b9956a0'
